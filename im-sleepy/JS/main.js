ac3a79e408a23efd5bfb711d29c4a560f12e8a18
inconveniencestore <iivyleaves@gmail.com>
2024-11-07 12:26:43 -0500

    sort works i just cant nuke stuff

--- im-sleepy/JS/main.js
+++ im-sleepy/JS/main.js
@@ -5,16 +5,34 @@ const DOMSelectors = {
   container: document.querySelector(".main-box"),
 };
 
-store.forEach((store) =>
-  DOMSelectors.container.insertAdjacentHTML(
-    "beforeend",
-    `  <div class= "container">
-    <h2>${store.object}</h2>
-    <img src="${store.imageUrl}" class="card-img" alt="">
-    <h3>${store.description}</h3>
-    <h4>$${store.price}</h4>
-  </div>`
-  )
-);
+//make into fucntion
 
-function filterItems(value) {}
+function createItems(store) {
+  store.forEach((value) =>
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `  <div class= "container">
+      <h2>${value.object}</h2>
+      <img src="${value.imageUrl}" class="card-img" alt="">
+      <h3>${value.description}</h3>
+      <h4>$${value.price}</h4>
+    </div>`
+    )
+  );
+}
+
+//includes
+function filterItems(store) {
+  let result = store.filter((value) => value.avaliability === "last one");
+  createItems(result);
+}
+
+//filter
+//sort
+//includes
+
+//filter
+function filterPrice(store) {}
+
+//createItems(store);
+filterItems(store);
