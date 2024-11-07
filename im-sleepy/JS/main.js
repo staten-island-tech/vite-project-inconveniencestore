86d879ae110ca456c1fd3a920d6f994aaf8b3561
inconveniencestore <iivyleaves@gmail.com>
2024-11-06 23:27:54 -0500

    i need to fix colors but for now wave has stopped yelling at me

--- im-sleepy/JS/main.js
+++ im-sleepy/JS/main.js
@@ -10,9 +10,11 @@ store.forEach((store) =>
     "beforeend",
     `  <div class= "container">
     <h2>${store.object}</h2>
-    <img src="${store.imageUrl}" alt="">
+    <img src="${store.imageUrl}" class="card-img" alt="">
     <h3>${store.description}</h3>
-    <h3>$${store.price}</h3>
+    <h4>$${store.price}</h4>
   </div>`
   )
 );
+
+function filterItems(value) {}
